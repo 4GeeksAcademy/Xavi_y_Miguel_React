46c9416f8fbd0109a86fd3dc9a4669e9a2690241
mitoperni <mitoperni@gmail.com>
2025-12-03 18:10:00 +0100

    Explicación Miguel 03/12/2025

---
+++ src/js/components/Alert.jsx
@@ -0,0 +1,13 @@
+const Alert = () => {
+  return (
+    <div>
+      <div className="alert alert-light mt-4 container" role="alert">
+        <h1>Warning no se que</h1>
+        <p>Más texto</p>
+        <button type="button" className="btn btn-primary">Soy un botón</button>
+      </div>
+    </div>
+  );
+};
+
+export default Alert;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,68 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./Navbar";
+import Alert from "./Alert";
+import Card from "./Card";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const cardsDetails = [
+		{
+			title: "Card title 1",
+			text: "Some quick example text to build on the card title and make up the bulk of the card’s content.",
+			imgSrc: "cualquierimagen1",
+			btnText: "Go somewhere 1",
+		},
+		{
+			title: "Card title 2",
+			text: "Some quick example text to build on the card title and make up the bulk of the card’s content.",
+			imgSrc: "cualquierimagen2",
+			btnText: "Go somewhere 2",
+		},
+		{
+			title: "Card title 3",
+			text: "Some quick example text to build on the card title and make up the bulk of the card’s content.",
+			imgSrc: "cualquierimagen3",
+			btnText: "Go somewhere 3",
+		},
+		{
+			title: "Card title 4",
+			text: "Some quick example text to build on the card title and make up the bulk of the card’s content.",
+			imgSrc: "cualquierimagen4",
+			btnText: "Go somewhere 4",
+		},
+	];
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+      <Alert />
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-3">
+            <Card />
+          </div>
+          <div className="col-12 col-md-3">
+            <Card />
+          </div>
+          <div className="col-12 col-md-3">
+            <Card />
+          </div>
+          <div className="col-12 col-md-3">
+            <Card />
+          </div>
+        </div>
+				<p className="mt-5">Estos cards han sido generados con un map:</p>
+        <div className="row">
+					{cardsDetails.map((card, index) => {
+						return (
+              <div className="col-12 col-md-3" key={index}>
+                <Card />
+              </div>
+            );
+					})}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
